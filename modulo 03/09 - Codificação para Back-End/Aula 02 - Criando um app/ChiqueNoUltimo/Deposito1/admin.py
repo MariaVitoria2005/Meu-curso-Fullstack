690bdbe00d022c71f780b8f646c88c25f9f17366
vitoria <mariavitoriamedeiros27@gmail.com>
2024-05-13 11:18:57 -0300

    meuprojeto

---
+++ modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/ChiqueNoUltimo/Deposito1/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(RoupaFemininas)
+admin.site.register(RoupaMasculina)
+admin.site.register(RoupaInfantil)
--- modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/LojaCarros/carros/models.py
+++ modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/LojaCarros/carros/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-class Fabricante(models.Model):
+# Create your models here.
+class Marca(models.Model):
     nome = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
 
-class Moto(models.Model):
-    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+class Carro(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     modelo = models.CharField(max_length=100)
     ano = models.IntegerField()
-    cilindrada = models.IntegerField()
+    cor = models.CharField(max_length=50)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.modelo
+        return f"{self.modelo} ({self.ano})"
