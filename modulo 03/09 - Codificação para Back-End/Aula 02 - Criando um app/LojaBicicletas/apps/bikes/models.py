9c04ef818dc1f26ca8d1af4cbc2c28eb23999659
vitoria <mariavitoriamedeiros27@gmail.com>
2024-05-14 11:05:58 -0300

    oiiii

--- modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/LojaBicicletas/apps/bikes/models.py
+++ modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/LojaBicicletas/apps/bikes/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=100)
-
+    
     def __str__(self):
         return self.nome
 
-class Carro(models.Model):
+class Bicicleta(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     modelo = models.CharField(max_length=100)
-    ano = models.IntegerField()
-    cor = models.CharField(max_length=50)
+    ano_fabricacao = models.IntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.modelo} ({self.ano})"
+        return f" {self.modelo} - {self.ano_fabricacao}"
