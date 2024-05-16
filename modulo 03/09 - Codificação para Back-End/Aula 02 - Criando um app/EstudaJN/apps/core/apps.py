8e8c1a5655e19764014d0f64d890b28234124df2
vitoria <mariavitoriamedeiros27@gmail.com>
2024-05-16 10:04:21 -0300

    16/05

--- modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/EstudaJN/apps/core/apps.py
+++ modulo 03/09 - Codificação para Back-End/Aula 02 - Criando um app/EstudaJN/apps/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MotosConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.Motos'
+    name = 'apps.core'
--- modulo 03/09 - Codificação para Back-End/Aula 03 - Models extras & Organização/PrimeiroProjeto/honda/Motos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Fabricante)
-admin.site.register(Moto)
