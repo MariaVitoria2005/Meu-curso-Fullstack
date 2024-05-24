1cbb3efe063115c7544493ec0a75c38942c086a5
vitoria <mariavitoriamedeiros27@gmail.com>
2024-05-24 11:24:28 -0300

    ola mundo

---
+++ modulo 03/09 - Codificação para Back-End/Treinamento/Blog/apps/post/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def espia_so(resquest):
+    return HttpResponse("o ridrigo é uma pessoa feliz!")
---
+++ modulo 03/09 - Codificação para Back-End/Treinamento/SegundoProjeto/apps/post/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def garagem(request):
+    return HttpResponse("Boa Tarde!")
