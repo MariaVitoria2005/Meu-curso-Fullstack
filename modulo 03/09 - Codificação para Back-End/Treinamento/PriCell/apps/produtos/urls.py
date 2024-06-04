f8264ec0a6e1a8af89af0905cb95115a170924aa
vitoria <mariavitoriamedeiros27@gmail.com>
2024-06-04 10:01:14 -0300

    aula 07/08

--- modulo 03/09 - Codificação para Back-End/Treinamento/PriCell/apps/produtos/urls.py
+++ modulo 03/09 - Codificação para Back-End/Treinamento/PriCell/apps/produtos/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', VerProdutos),
+    path("", LinkInicial, name="pagina_index"),
+	path("cadastro", LinkCadastro, name="pagina_cadastro")
 ]
--- modulo 03/09 - Codificação para Back-End/Treinamento/PriCell/apps/produtos/views.py
+++ modulo 03/09 - Codificação para Back-End/Treinamento/PriCell/apps/produtos/views.py
@@ -5,3 +5,9 @@ def VerProdutos(request):
     produtos_lista = produto.objects.all()
     print(produtos_lista)
     return render(request,'index.html',{'produtos':produtos_lista})
+
+def LinkInicial(request):
+    return render(request, "index.html")
+
+def LinkCadastro(request):
+    return render(request, "cadastro.html")
