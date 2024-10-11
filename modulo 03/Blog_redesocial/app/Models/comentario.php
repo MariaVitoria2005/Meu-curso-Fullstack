d454e0a465e7b196fce7e1fcea8fa3ed46e80de3
MariaVitoria2005 <mariavitoriamedeiros27@gmail.com>
2024-10-11 11:20:01 -0300

    ajustes

--- modulo 03/Blog_redesocial/app/Models/comentario.php
+++ modulo 03/Blog_redesocial/app/Models/comentario.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class comentario extends Model
 {
     use HasFactory;
+    protected $fillable=['conteudo','data_comentario'];
+    public function users(){
+        return $this->bolongsTo(user::class);
+    }
+    public function postagem(){
+        return $this->bolongsTo(user::class);
+    }
 }
--- modulo 03/Blog_redesocial/app/Models/postagem.php
+++ modulo 03/Blog_redesocial/app/Models/postagem.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class postagem extends Model
 {
     use HasFactory;
+    protected $fillable=['titulo','conteudo','data_postagem','foto'];
+    public function users(){
+        return $this->bolongsTo(user::class);
+    }
 }
